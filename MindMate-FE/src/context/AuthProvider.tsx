5393436c862d64825e2005c131f1d21560a15be6
prathmesh-ghatmal <mrjpg2311@gmail.com>
2025-08-08 11:12:51 +0000

    fix: add tags in edit journal

--- MindMate-FE/src/context/AuthProvider.tsx
+++ MindMate-FE/src/context/AuthProvider.tsx
@@ -4,9 +4,10 @@ import { useDispatch, useSelector } from "react-redux"
 import { loginSuccess, logout } from "@/redux/slices/authSlice"
 import type { RootState, AppDispatch } from "@/redux/store"
 import type { User } from "@/types/User"
-import { fetchUserProfile, loginUser, registerUser } from "@/services/authService"
+import { fetchUserProfile, getGoogleAuthUrl, loginUser, registerUser } from "@/services/authService"
 import { toast } from "react-hot-toast"
 import { useNavigate } from "react-router-dom" 
+
 
 interface AuthContextType {
   user: User | null
@@ -14,6 +15,7 @@ interface AuthContextType {
   signup: (email: string, password: string, name: string) => Promise<void>
   socialLogin: (provider: string) => Promise<void>
   logout: () => void
+  googlelogin: (access_token: string, refresh_token: string) => Promise<void>
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
@@ -70,14 +72,21 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   }
 
   const socialLogin = async (provider: string) => {
-    // Mock logic only unless backend OAuth is implemented
-    const mockUser: User = {
-      id: "social-1",
-      email: `user@${provider}.com`,
-      is_verified: true,
-      first_name: `${provider}User`,
+     try {
+      console.log(provider)
+      const {auth_url} = await getGoogleAuthUrl(  );
+      window.location.href = auth_url;
+    } catch (err) {
+      console.error("Failed to get auth URL", err);
     }
-    dispatch(loginSuccess(mockUser))
+    // Mock logic only unless backend OAuth is implemented
+    // const mockUser: User = {
+    //   id: "social-1",
+    //   email: `user@${provider}.com`,
+    //   is_verified: true,
+    //   first_name: `${provider}User`,
+    // }
+    // dispatch(loginSuccess(mockUser))
   }
 
   const handleLogout = () => {
@@ -88,8 +97,39 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     toast.success("Logged out successfully.")
   }
 
+  const googlelogin = async (access_token: string, refresh_token: string) => {
+    try {
+      
+      console.log(access_token, refresh_token)
+      if (access_token && refresh_token) {
+        localStorage.setItem("access_token", access_token)
+        localStorage.setItem("refresh_token", refresh_token)
+
+        // fetchUserProfile uses token from interceptor
+        const userProfile = await fetchUserProfile()
+        const user = {
+          ...userProfile,
+          access_token,
+          refresh_token,
+        }
+
+        dispatch(loginSuccess(user))
+        toast.success("Login successful!")
+      } else {
+        console.error("Access token is missing.")
+        toast.error("Unexpected error during login. Try again.")
+      }
+    } catch (err: any) {
+      if (err?.response?.status === 403 && err?.response?.data?.detail === "Please verify your email first") {
+        toast.error("Please verify your email before logging in.")
+      } else {
+        toast.error("Login failed. Please check your credentials.")
+      }
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user, login, signup, socialLogin, logout: handleLogout }}>
+    <AuthContext.Provider value={{ user, login, signup, socialLogin, logout: handleLogout,googlelogin }}>
       {children}
     </AuthContext.Provider>
   )
@@ -102,3 +142,4 @@ export const useAuth = () => {
   }
   return context
 }
+
